cdab7ca709b6d609e2cff39842f7db911ba26134
Tejasri <ep23btech11018@iith.ac.in>
2025-06-24 14:31:22 +0530

    Added signin page

---
+++ lib/pages/RoomSwap.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RoomSwapPage extends StatelessWidget {
+  const RoomSwapPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Room Swap Request')),
+      body: const Center(child: Text('Room Swap Request Page')),
+    );
+  }
+}
